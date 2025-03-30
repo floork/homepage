811b3ae011c0e81bea6d34f857bae53481ed1959
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-03-30 11:21:22 +0200

    new homepage design

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Homepage development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = with pkgs; [
+            bundler
+            bundix
+            jekyll
+            ruby
+          ];
+        };
+      }
+    );
+}
